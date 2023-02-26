39e1d20104bd9046ed9f1a2f6c9639db562a570a
zzhe <zzh31215@gmail.com>
2023-02-27 00:54:34 +0800

    feat: 增加登录白名单

--- app/middleware/AuthMiddleware/constant.ts
+++ app/middleware/AuthMiddleware/constant.ts
@@ -1,3 +1,15 @@
 export const whiteListRoutes = [
-  '/user/login.json'
+  /* 前台接口 */
+  '/article/getArticleList.json',
+  '/article/queryType.json',
+  '/article/queryTag.json',
+  '/article/queryOne.json',
+  '/nav/query.json',
+  '/recommendation/queryRecommendation.json',
+  '/statistic/updateViewCounts.json',
+  '/article/updateViews.json',
+  '/mysite/tracert-field-map.json',
+
+  /* 后台接口 */
+  '/user/login.json',
 ]
